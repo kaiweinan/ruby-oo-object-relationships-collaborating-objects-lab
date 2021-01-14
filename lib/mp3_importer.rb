49a6244b2954d05749e2f6a2814d5c8d1e864a35
kaiweinan <kaiweinan@yahoo.com>
2021-01-13 19:21:23 -0500

    commit tree 35327e9be7a808722456fb3945e30a2e3855575b

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,8 +1,18 @@
 class MP3Importer
-    attr_accessor :name
+    attr_accessor :path
     
-    def initialize(name)
-    @name = name
+    def initialize(filepath)
+    @path = filepath
     end
     
+    def files
+    @files = Dir.entries(@path)
+    @files.delete_if { |file|  file == "." || file == ".."}
+    end
+
+    def import
+    
+        files.each { |f| Song.new_by_filename(f) }
+
+    end
     end
--- lib/song.rb
+++ lib/song.rb
@@ -15,12 +15,16 @@ class Song
         
     end
     
-    def self.new_by_filename(filename)
-        song = self.new
-        binding.pry 
-        song.artist= filename.split(" - ")[1]
-        song
-    
-      end
+    def self.new_by_filename(file_name)
+        name = file_name.split(" - ")
+        song = self.new(name[1])
+        song.artist = Artist.find_or_create_by_name(name[0])
+        @@all << song
+        return song
+    end
 
+    def artist_name=(name)
+        artist = Artist.find_or_create_by_name(name)
+        self.artist = artist
+    end 
     end
